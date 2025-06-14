3bb34b372b69f450957e113736e20bdc382fb422
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-14 14:13:09 -0700

    create login component

--- react-router-dom/test/src/App.jsx
+++ react-router-dom/test/src/App.jsx
@@ -4,18 +4,16 @@ import About from './About'
 import Contact from './Contact'
 import Protected from './Protected'
 import OnlyAuthed from './OnlyAuthed'
-
-// // CALL COMPONENTS HERE
-
-
-
-
+import Login from './Login'
+import { useState } from 'react'
 
 function App(){
     const login=true
+    const [userLoggedIn, setUserLoggedIn] = useState(false)
     return(
         <BrowserRouter >
         <nav>
+            <Link to='/login'>Login</Link>
             <Link to='/'>Home</Link>
             <Link to='/about'>About</Link>
             <Link to='/contact'>Contact</Link>
@@ -23,9 +21,10 @@ function App(){
         </nav>
 
         <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/about' element={<About />}> </Route>
-            <Route path='/contact' element={<Contact />}> </Route>
+            <Route path='/login' element={<Login setUserLoggedIn={setUserLoggedIn}/>}/>
+            <Route path='/' element={<Home />}/>
+            <Route path='/about' element={<About />}/>
+            <Route path='/contact' element={<Contact />}/>
             <Route path='/authed' element={<Protected login={login}> <OnlyAuthed /></Protected>}/>
             <Route path='*' element={<h1>404 Page Not Found</h1>} />
         </Routes>
---
+++ react-router-dom/test/src/Login.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import {useNavigate} from "react-router-dom"
+
+function Login({setUserLoggedIn}){
+    //setting states for usernames and passwords
+    const [username, setUsername] = useState("") 
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(false)
+
+    const navigate = useNavigate() // using use navigate to send users to a different route if able to authenticate
+
+    const handleLogin = (e) =>{
+        e.preventDefault() // prevents page reload when submitting a form
+         if(username === "iamadmin" && password === "Admin123"){ 
+        setUserLoggedIn(true)
+        navigate('/') // navigates user to the root if logged in
+    }else{
+        setError(true)
+    }
+    }
+   
+return(
+    <div>
+        <h2>Login Page:</h2>
+        <form onSubmit={handleLogin}>
+            <div>
+                <h3>Username:</h3>
+                <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} placeholder="Enter your username ..."></input>
+            </div>
+            <div>
+                <h3>Password:</h3>
+                <input type="text" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="Enter password ..."></input>
+            </div>
+
+            <button type="submit">Log In</button>
+        </form>
+        {error && <p>Login failed!</p>}
+    </div>
+)
+}
+
+export default Login
