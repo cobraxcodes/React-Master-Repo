0fb79b7c49a8332df826037e0141411e28de938a
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-14 12:32:05 -0700

    create react-router-dom practice sandbox

---
+++ react-router-dom/src/App.jsx
@@ -0,0 +1,28 @@
+// import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+
+// // CALL COMPONENTS HERE
+// //ex:
+
+// // function Home(){
+// //   return (
+// //     <Home />
+// //   )
+// // }
+
+// function App(){
+//   return(
+//    <BrowserRouter> // BROWSES THROUGH ROUTES
+//    <nav>
+//     <Link to = '/'>Home</Link> 
+//     <Link to='/About'>About</Link>
+//    </nav>
+
+//    <Routes>
+//     <Route path='/' element={<Home />}></Route> // matches the link = path, and renders the component through element 
+//     <Route path='/About' element={<About />}></Route>
+//    </Routes>
+//    </BrowserRouter>
+//   )
+// }
+
+// export default App
