a2bcc2070af54eb46647c8dc701a8204d3f02d61
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-15 16:04:00 -0700

    add pet list component

--- react-router-dom/pet-adoption/src/App.jsx
+++ react-router-dom/pet-adoption/src/App.jsx
@@ -1,23 +1,38 @@
-// 📜 4️⃣ Mini Project: Pet Adoption Center 🐶
-// 📃 Home.jsx
-
-// 📃 AdoptablePets.jsx
-
-// 📃 PetProfile.jsx
-
-// 📃 AdoptionForm.jsx (protected)
-
-// 📃 Login.jsx
+// Mini Project: Pet Adoption Center 
+//  Home.jsx 1. done
+// Pets.jsx 2.
+// login.jsx 4.
+//  PetProfile.jsx 3.
+// Protect.jsx 5.
+// AdoptionForm.jsx (protected) 6.
+// Login.jsx
 
 // Routes:
-
-// /
-
-// /pets
-
-// /pets/:id
-
-// /pets/:id/adopt (protected)
-
 // Why?
 // Protected nested route to guard adoption form.
+
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Home from './Home'
+import Pets from './Pets'
+
+function App(){
+    return(
+       <div>
+        <BrowserRouter>
+        <Link to='/'>Home</Link>
+        <Link to='/pets'>Pets</Link>
+
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/pets' element={<Pets />}/>
+        </Routes>
+        
+        
+        
+        
+        </BrowserRouter>
+       </div>
+    )
+}
+
+export default App
---
+++ react-router-dom/pet-adoption/src/Home.jsx
@@ -0,0 +1,14 @@
+
+function Home(){
+    return(
+        <div>
+            <h1>Welcome to Dog Haven!</h1>
+            <p>Home to doggies that needs a new home!</p>
+            <div>
+                <img src="https://media.istockphoto.com/id/467923438/photo/silly-dog-tilts-head-in-front-of-barn.jpg?s=612x612&w=0&k=20&c=haPwfoPl_ggvNKAga_Qv4r88qWdcpH-qZ5DaBba6-8U="/>
+            </div>
+        </div>
+    )
+}
+
+export default Home
