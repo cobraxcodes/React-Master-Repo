e884ceb3f2a4c358328f32914a3cc86ce06a4196
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-15 14:30:34 -0700

    add home and events component

--- react-router-dom/movie-watchlist/src/App.jsx
+++ react-router-dom/movie-watchlist/src/App.jsx
@@ -1,22 +1,36 @@
-// 📃 Home.jsx
-
-// 📃 Movies.jsx
-
-// 📃 MovieDetails.jsx (protected)
-
-// 📃 Favorites.jsx (protected)
-
-// 📃 Login.jsx
-
-// Routes:
-
-// /
-
-// /movies
-
-// /movies/:id
-
-// /favorites → only visible if logged in
+//  Home.jsx 1.
+//  Movies.jsx 2. 
+// PROTECTED 4.
+//  MovieDetails.jsx (protected) 5.
+// Favorites.jsx (protected) 6.
+//  Login.jsx 3.
 
 // Why?
 // Secure Favorites route and use useParams for movie details.
+
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Home from './Home'
+import Events from './Movies'
+
+function App(){
+    return(
+        <div>
+            <BrowserRouter>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/events'>Events</Link>
+            </nav>
+
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/events' element={<Events />} />
+
+            </Routes>
+            
+            
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App
