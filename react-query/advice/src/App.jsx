ed29e62f590ae166479eacafe17197bf8fe1aca5
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-19 14:48:14 -0700

    feat: create global variable for login functionality

--- react-query/advice/src/App.jsx
+++ react-query/advice/src/App.jsx
@@ -4,20 +4,23 @@
 // Show: Advice text
 
 // Home Component - done
-// context api wrapper
+// context api wrapper - done
 // Login Component
 // protect component
 // react query wrapper
 // create a joke component with button
 
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
-
-
+import { useState } from 'react';
+import { Context } from './Context';
 import Home from "./Home";
+import { useState } from 'react';
 
 
 function App(){
+    const [userLogin, setUserLogin] = useState(false)
     return(
+        <Context.Provider value={{userLogin,setUserLogin}}>
         <BrowserRouter>
         <nav class='nav'>
               <Link to='/'>Home</Link>
@@ -27,7 +30,8 @@ function App(){
             <Route path='/' element={<Home />}/>
         </Routes>
         </BrowserRouter>
-      
+        </Context.Provider>
+     
     )
 }
 
