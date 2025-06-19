4a6259b3ad062c453cd8cd1bfc45b30b142a2d31
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-19 14:44:49 -0700

    feat: add home component with nav

--- react-query/advice/src/App.jsx
+++ react-query/advice/src/App.jsx
@@ -1,7 +1,34 @@
 // Random Advice
-
 // Button: Get Advice
-
 // API: Advice Slip API
-
 // Show: Advice text
+
+// Home Component - done
+// context api wrapper
+// Login Component
+// protect component
+// react query wrapper
+// create a joke component with button
+
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+
+
+import Home from "./Home";
+
+
+function App(){
+    return(
+        <BrowserRouter>
+        <nav class='nav'>
+              <Link to='/'>Home</Link>
+        </nav>
+          
+        <Routes>
+            <Route path='/' element={<Home />}/>
+        </Routes>
+        </BrowserRouter>
+      
+    )
+}
+
+export default App
---
+++ react-query/advice/src/Home.jsx
@@ -0,0 +1,13 @@
+
+
+function Home(){
+return(
+    <div>
+        <h1>Welcome!</h1>
+        <h3>Advice Needed?</h3>
+        <p>Login to get started!</p>
+    </div>
+)
+}
+
+export default Home
