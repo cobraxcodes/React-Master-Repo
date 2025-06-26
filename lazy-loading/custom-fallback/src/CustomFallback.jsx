178924973e6a4256154a11666689a0a4466e961d
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-26 13:28:53 -0700

    add custom fallback and use lazy loading

---
+++ lazy-loading/custom-fallback/src/CustomFallback.jsx
@@ -0,0 +1,11 @@
+import { Spinner } from '@material-tailwind/react'
+
+function CustomFallback(){
+    return(
+        <div>
+            <Spinner color='blue'/>
+        </div>
+    )
+}
+
+export default CustomFallback
---
+++ lazy-loading/custom-fallback/src/Heavy.jsx
@@ -0,0 +1,11 @@
+
+
+function Heavy(){
+    return(
+        <div>
+            <h1>I am a heavy component. Lazy load me!</h1>
+        </div>
+    )
+}
+
+export default Heavy
