2bb623755c2b402a04033ef8898213abcf939d9a
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-07-01 20:34:25 -0700

    test: add test for counter

--- unit-testing/button-counter/src/Greet.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Greet from './Greet'
-
-test('renders greeting text correctly', () =>{ // defining test case, and test case name
-    render (<Greet/>);
-    expect(screen.getByText(/Hello World!/i)).toBeInTheDocument();
-})
