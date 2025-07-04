f4d74d49c994e8701ea8351558055c1e94a05c12
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-07-03 19:43:04 -0700

    test: add unit testing for basic-equality

--- unit-testing/test/jest.setup.js
+++ unit-testing/test/jest.setup.js
@@ -1,1 +1,8 @@
-import '@testing-library/jest-dom';
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    // if you use aliases like @/components in vite.config.js, map them here
+    "^@/(.*)$": "<rootDir>/src/$1"
+  }
+};
