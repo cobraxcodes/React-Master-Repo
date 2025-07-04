1530e10ff48f4b01c670c3e6b9b854ed0aa0d9de
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-07-03 21:56:35 -0700

    add unit testing using .toBeTruthy() and .toBeFalsy()

--- unit-testing/boolean/src/App.jsx
+++ unit-testing/boolean/src/App.jsx
@@ -2,3 +2,7 @@
 // Write a function isAdult(age) that returns true if age >= 18.
 // Test it with both a truthy and falsy value.
 
+import React from 'react'
+export function IsAdult(age){
+    return age>=18
+}
