3e5288051f06127592a4caa50d9358bb78e3b5b3
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-07-03 23:38:39 -0700

    add scaffold unit testing

---
+++ unit-testing/each-reset/App.test.js
@@ -0,0 +1,6 @@
+// 📝 Challenge 6: beforeEach() Reset
+// Declare let counter = 0.
+// In a beforeEach, reset counter = 0.
+// In one test, increment counter and check it’s 1.
+// In another test, confirm it resets to 0.
+
--- unit-testing/each-reset/src/App.jsx
+++
@@ -1,5 +0,0 @@
-// 📝 Challenge 6: beforeEach() Reset
-// Declare let counter = 0.
-// In a beforeEach, reset counter = 0.
-// In one test, increment counter and check it’s 1.
-// In another test, confirm it resets to 0.
