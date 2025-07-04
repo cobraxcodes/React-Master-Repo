c0aca621ee4181ca42a7bafa34dbef8aaaa54234
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-07-03 23:46:22 -0700

    add unit-testing for reset use beforEach()

--- unit-testing/each-reset/App.test.js
+++ unit-testing/each-reset/App.test.js
@@ -4,3 +4,24 @@
 // In one test, increment counter and check it’s 1.
 // In another test, confirm it resets to 0.
 
+import '@testing-library/jest-dom'
+
+let counter;
+
+beforeEach(() =>{
+    counter = 0
+})
+
+test('counter increments by 1', () =>{
+    counter++
+    expect(counter).toBe(1) // use to be for value expectations
+})
+
+test('counter reset to 0', ()=>{
+    expect(counter).toBe(0)
+})
+
+test('counter decrements by 1', () =>{
+    counter--
+    expect(counter).toBe(-1)
+})
